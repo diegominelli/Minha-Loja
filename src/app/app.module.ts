450220a6c1216cc55b0c0979916d8a0e905c98de
diegominelli <diego.minelli@hotmail.com>
2021-01-07 16:08:09 -0300

    Modulos compartilhados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,11 +13,8 @@ import { CustomFormsModule } from 'ng2-validation'
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
 registerLocaleData(localePt);
- 
+
 import { AppComponent } from './app.component';
-import { MenuComponent } from './navegacao/menu/menu.component';
-import { HomeComponent } from './navegacao/home/home.component';
-import { FooterComponent } from './navegacao/footer/footer.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
 import { ContatoComponent } from './institucional/contato/contato.component';
 import { rootRouterConfig } from './app.routes';
@@ -25,13 +22,11 @@ import { DataBindingComponent } from './demos/data-binding/data-binding.componen
 import { ProdutoService } from './produtos/produtos.service';
 import { ListaProdutoComponent } from './produtos/lista-produto/lista-produto.component';
 import { CadastroComponent } from './demos/reactiveForms/cadastro/cadastro.component';
+import { NavegacaoModule } from './navegacao/navegacao.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent,
-    HomeComponent,
-    FooterComponent,
     SobreComponent,
     ContatoComponent,
     DataBindingComponent,
@@ -43,6 +38,7 @@ import { CadastroComponent } from './demos/reactiveForms/cadastro/cadastro.compo
     FormsModule,
     HttpClientModule,
     ReactiveFormsModule,
+    NavegacaoModule,
     NgBrazil,
     TextMaskModule,
     CustomFormsModule,
