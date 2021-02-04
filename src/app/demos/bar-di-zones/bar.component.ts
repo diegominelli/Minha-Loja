645762606c8c5c5a2564188d292f150c11306747
diegominelli <diego.minelli@hotmail.com>
2021-02-04 14:42:36 -0300

    Providers-UseExisting

--- src/app/demos/bar-di-zones/bar.component.ts
+++ src/app/demos/bar-di-zones/bar.component.ts
@@ -1,19 +1,20 @@
 import { Component, Inject, Injector, OnInit } from '@angular/core';
 import { BarUnidadeConfig, BAR_UNIDADE_CONFIG } from './bar.config';
-import { BarFactory, BarServices, BarServicesMock } from './bar.service';
+import { BarFactory, BarServices, BarServicesMock, BebidaService } from './bar.service';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-bar',
   templateUrl: './bar.component.html',
   providers: [
-    // { provide: BarServices, useClass: BarServicesMock },
+    { provide: BarServices, useClass: BarServices },
     {
       provide: BarServices, useFactory: BarFactory,
       deps: [
         HttpClient, Injector
       ]
-    }
+    },
+    { provide: BebidaService, useExisting: BarServices }
   ]
 })
 export class BarComponent implements OnInit {
@@ -21,20 +22,23 @@ export class BarComponent implements OnInit {
   ConfigManual: BarUnidadeConfig;
   Config: BarUnidadeConfig;
   barBebida1: string;
+  barBebida2: string;
   dadosUnidade: string;
 
   constructor(
     private barServices: BarServices,
     @Inject('ConfigManualUnidade') private ApiConfigManual: BarUnidadeConfig,
     @Inject(BAR_UNIDADE_CONFIG) private ApiConfig: BarUnidadeConfig,
+    private bebidaService: BebidaService
     ) { }
 
   ngOnInit(): void { 
     this.barBebida1 = this.barServices.obterBebidas();
     this.ConfigManual = this.ApiConfigManual;
     this.Config = this.ApiConfig;
-
     this.dadosUnidade = this.barServices.obterUnidades();
+    
+    this.barBebida2 = this.bebidaService.obterBebidas();
   }
 
 }
