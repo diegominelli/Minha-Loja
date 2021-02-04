f9141c3b40a079763163e1460f80f3e5203cc780
diegominelli <diego.minelli@hotmail.com>
2021-02-04 14:31:47 -0300

    Providers-UseFactory

--- src/app/demos/bar-di-zones/bar.service.ts
+++ src/app/demos/bar-di-zones/bar.service.ts
@@ -1,11 +1,23 @@
+import { Inject, Injectable, Injector} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Injectable} from '@angular/core';
+import { BAR_UNIDADE_CONFIG, BarUnidadeConfig } from './bar.config';
+
+export function BarFactory(http: HttpClient, injector: Injector) {
+
+  return new BarServices(http, injector.get(BAR_UNIDADE_CONFIG));
+}
 
 @Injectable()
 export class BarServices {
 
   constructor(
-    private http: HttpClient) { }
+    private http: HttpClient,
+    @Inject(BAR_UNIDADE_CONFIG) private config: BarUnidadeConfig
+    ) { }
+
+    public obterUnidades(): string {
+      return 'Unidade ID: ' + this.config.unidadeId + ' Token ' + this.config.unidadeToken
+    }
 
   obterBebidas(): string {
     return 'Bebidas';
